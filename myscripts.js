171d4b628fdd32f75894b2a22dc64b42f39a8ac6
Kyle Mishler <44989552+mishper@users.noreply.github.com>
2018-11-28 21:50:15 -0600

    First Update
    Adding HUD elements and fleshing out core mechanics the player now has a fuel tank and uses fuel for each move. Some tiles contain more fuel which can be collected to allowed continued exploration.

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,12 @@
 var zoom = 32; //number of tiles in a row
 var grid = 25; //width of each tile in pixels
 var world;
+var hud;
 
 function init() {
 	world = new World();
-	world.update(); //call update to show visible tiles at the start
+	hud = new HUD();
+	world.update(); //call update to display initialized game state
 }
 function move(dir) { //this is essentially the main game loop
 	world.player.move(dir);
@@ -32,6 +34,23 @@ class World {
 		for(var i = 0; i < this.tiles.length; i++){
 			this.tiles[i].update();
 		}
+		this.player.update();
+		hud.update();
+	}
+}
+class HUD {
+	constructor(){
+		this.fuelGauge = document.getElementById("amount");
+		this.infoCont = document.getElementById("infoCont");	
+	}
+	update(){
+		this.fuelGauge.style.height = String(world.player.fuel/world.player.tankSize*100)+"%";
+		var tile = world.tiles[world.player.x+world.player.y*zoom];
+		if(tile.content.length > 0){
+			this.infoCont.innerHTML = "<div class='tileItem'><div class='tileItemName'>Fuel</div><div onClick='world.player.addItem(0)' class='tileItemAdd'>+</div></div>";
+		}else{
+			this.infoCont.innerHTML = "<p>Looks like nothings here<p>";
+		}
 	}
 }
 class Tile {
@@ -43,15 +62,34 @@ class Tile {
 		this.isVisible = false;
 		this.wasVisible = false;
 		this.elem.style.backgroundColor = this.c;
+		this.content = [];
+		if(Math.random() > .9){
+			var fuelSpot = new TileContent();
+			this.content.push(fuelSpot);
+			this.elem.appendChild(fuelSpot.elem);
+		}
 	}
 	update(){
 		if(this.isVisible){
-			console.log("vis");
 			this.elem.style.opacity = 1;
 			this.wasVisible = true;
 		}else if(this.wasVisible){
 			this.elem.style.opacity = .25;
 		}
+	}
+	updateContent(){
+		if(this.content.length > 0){
+		
+		}else{
+			this.elem.innerHTML = "";
+		}
+	}
+}
+class TileContent {
+	constructor(){
+		this.fuel = 30;
+		this.elem = document.createElement("div");
+		this.elem.className = "fuelSpot";	
 	}
 }
 class Player {
@@ -61,21 +99,35 @@ class Player {
 		this.elem = document.getElementById("player");
 		this.elem.style.top = this.y*grid+"px";
 		this.elem.style.left = this.x*grid+"px";
+		this.tankSize = 150;
+		this.fuel = 100;
+		this.moveCost = 10;
+	}
+	update(){}
+	addItem(i){
+		var tile = world.tiles[this.x+this.y*zoom];
+		this.fuel += tile.content[i].fuel;
+		tile.content.splice(i,1);
+		tile.updateContent();
+		hud.update();
 	}
 	move(dir){
-		switch(dir){
-			case "up":
-				this.y--;
-				break;
-			case "left":
-				this.x--;
-				break;
-			case "right":
-				this.x++;
-				break;
-			case "down":
-				this.y++;
-				break;
+		if(this.fuel > this.moveCost){
+			this.fuel -= this.moveCost;
+			switch(dir){
+				case "up":
+					this.y--;
+					break;
+				case "left":
+					this.x--;
+					break;
+				case "right":
+					this.x++;
+					break;
+				case "down":
+					this.y++;
+					break;
+			}
 		}
 	}
 	look(){
